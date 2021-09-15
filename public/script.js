9fd0fe3bc4d31fa8cdf891067ec21ed56f887bf8
GabrielMoretti <gabrielmorettisilva@gmail.com>
2021-09-15 14:06:27 -0300

    Categories Hover

--- public/script.js
+++ public/script.js
@@ -2,7 +2,14 @@ let cards = document.querySelectorAll('.card');
 let modal = document.querySelector('.modal');
 let close = document.querySelector('#close');
 let modalImg = document.querySelector('#modalImg')
+let file = document.querySelector('#file');
+let add = document.querySelector('#add');
+let remove = document.querySelector('#remove');
+let edit = document.querySelector('#edit');
+let body = document.querySelector('body');
+let main = document.querySelector('main');
 let source = "";
+
 
 function addModal(){
   if(modal.classList.contains('active')){
@@ -29,3 +36,11 @@ for(let i =0; i< cards.length;i++){
   });
 
 }
+
+add.addEventListener('click', ()=>{
+  file.click();
+})
+
+remove.addEventListener('click', () => {
+  main.classList.add('remove')
+})
